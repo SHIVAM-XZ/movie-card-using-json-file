f05a7fb0720aa8a485faf65f303321275322b8cc
SHIVAM-XZ <soni.shivam0037@gmail.com>
2024-08-01 22:37:39 +0530

    1 august

---
+++ movie.js
@@ -0,0 +1,367 @@
+const jsonData = `{
+    "dates": {
+      "maximum": "2024-08-28",
+      "minimum": "2024-08-07"
+    },
+    "page": 1,
+    "results": [
+      {
+        "adult": false,
+        "backdrop_path": "/5g2n3ilC8DpYv4diJeuQ1vKG2Kb.jpg",
+        "genre_ids": [16, 10751, 35, 28],
+        "id": 519182,
+        "original_language": "en",
+        "original_title": "Despicable Me 4",
+        "overview": "Gru and Lucy and their girls — Margo, Edith and Agnes — welcome a new member to the Gru family, Gru Jr., who is intent on tormenting his dad. Meanwhile, Gru faces a new nemesis in Maxime Le Mal and his femme fatale girlfriend Valentina, forcing the family to go on the run.",
+        "popularity": 2736.441,
+        "poster_path": "/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
+        "release_date": "2024-06-20",
+        "title": "Despicable Me 4",
+        "video": false,
+        "vote_average": 7.206,
+        "vote_count": 471
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/Akv9GlCCMrzcDkVz4ad8MdLl9DK.jpg",
+        "genre_ids": [16, 35, 10751, 12],
+        "id": 748783,
+        "original_language": "en",
+        "original_title": "The Garfield Movie",
+        "overview": "Garfield, the world-famous, Monday-hating, lasagna-loving indoor cat, is about to have a wild outdoor adventure! After an unexpected reunion with his long-lost father – scruffy street cat Vic – Garfield and his canine friend Odie are forced from their perfectly pampered life into joining Vic in a hilarious, high-stakes heist.",
+        "popularity": 1343.036,
+        "poster_path": "/p6AbOJvMQhBmffd0PIv0u8ghWeY.jpg",
+        "release_date": "2024-04-30",
+        "title": "The Garfield Movie",
+        "video": false,
+        "vote_average": 7.205,
+        "vote_count": 628
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/8fNBsXpgMvqk75SPsZzTmIqSRxY.jpg",
+        "genre_ids": [27, 53],
+        "id": 646683,
+        "original_language": "en",
+        "original_title": "The Exorcism",
+        "overview": "A troubled actor begins to unravel while shooting a supernatural horror film, leading his estranged daughter to wonder if he's slipping back into his past addictions or if there's something more sinister at play.",
+        "popularity": 579.436,
+        "poster_path": "/ar2h87jlTfMlrDZefR3VFz1SfgH.jpg",
+        "release_date": "2024-05-30",
+        "title": "The Exorcism",
+        "video": false,
+        "vote_average": 4.555,
+        "vote_count": 91
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/qwhnq8pkFEAFXRwmscISOKK90Mu.jpg",
+        "genre_ids": [28, 53],
+        "id": 560016,
+        "original_language": "en",
+        "original_title": "Monkey Man",
+        "overview": "Kid is an anonymous young man who ekes out a meager living in an underground fight club where, night after night, wearing a gorilla mask, he is beaten bloody by more popular fighters for cash. After years of suppressed rage, Kid discovers a way to infiltrate the enclave of the city’s sinister elite. As his childhood trauma boils over, his mysteriously scarred hands unleash an explosive campaign of retribution to settle the score with the men who took everything from him.",
+        "popularity": 462.676,
+        "poster_path": "/4lhR4L2vzzjl68P1zJyCH755Oz4.jpg",
+        "release_date": "2024-04-03",
+        "title": "Monkey Man",
+        "video": false,
+        "vote_average": 7.036,
+        "vote_count": 661
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/wsgni4E1UnBuvFouUUanSdniQi0.jpg",
+        "genre_ids": [28, 12, 18],
+        "id": 718821,
+        "original_language": "en",
+        "original_title": "Twisters",
+        "overview": "As storm season intensifies, the paths of former storm chaser Kate Carter and reckless social-media superstar Tyler Owens collide when terrifying phenomena never seen before are unleashed. The pair and their competing teams find themselves squarely in the paths of multiple storm systems converging over central Oklahoma in the fight of their lives.",
+        "popularity": 485.383,
+        "poster_path": "/mofrsXOCGXDHPYLaubKpBcb1YHt.jpg",
+        "release_date": "2024-07-10",
+        "title": "Twisters",
+        "video": false,
+        "vote_average": 7.214,
+        "vote_count": 281
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/H5HjE7Xb9N09rbWn1zBfxgI8uz.jpg",
+        "genre_ids": [28, 35],
+        "id": 746036,
+        "original_language": "en",
+        "original_title": "The Fall Guy",
+        "overview": "Fresh off an almost career-ending accident, stuntman Colt Seavers has to track down a missing movie star, solve a conspiracy and try to win back the love of his life while still doing his day job.",
+        "popularity": 375.264,
+        "poster_path": "/tSz1qsmSJon0rqjHBxXZmrotuse.jpg",
+        "release_date": "2024-04-24",
+        "title": "The Fall Guy",
+        "video": false,
+        "vote_average": 7.204,
+        "vote_count": 1703
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/hofnlIyF6bePkgQOpcuRWLvzf15.jpg",
+        "genre_ids": [16, 10751, 14],
+        "id": 14836,
+        "original_language": "en",
+        "original_title": "Coraline",
+        "overview": "Wandering her rambling old house in her boring new town, 11-year-old Coraline discovers a hidden door to a strangely idealized version of her life. In order to stay in the fantasy, she must make a frighteningly real sacrifice.",
+        "popularity": 231.848,
+        "poster_path": "/4jeFXQYytChdZYE9JYO7Un87IlW.jpg",
+        "release_date": "2009-02-05",
+        "title": "Coraline",
+        "video": false,
+        "vote_average": 7.874,
+        "vote_count": 7664
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/psheU3eQROaYZ0HCbZ0QdbqsiWq.jpg",
+        "genre_ids": [10749, 878, 18],
+        "id": 914206,
+        "original_language": "fr",
+        "original_title": "La Bête",
+        "overview": "In the near future where emotions have become a threat, Gabrielle finally decides to purify her DNA in a machine that will immerse her in her past lives and rid her of any strong feelings. She then meets Louis and feels a powerful connection, as if she had known him forever.",
+        "popularity": 303.443,
+        "poster_path": "/o42BrPukzRXlDi9KBCdClNqhlEr.jpg",
+        "release_date": "2024-02-07",
+        "title": "The Beast",
+        "video": false,
+        "vote_average": 6.279,
+        "vote_count": 70
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/kZbTOcTrEGyroQMWQSGIRlNSkwP.jpg",
+        "genre_ids": [878, 28, 14, 12],
+        "id": 626412,
+        "original_language": "ko",
+        "original_title": "외계+인 2부",
+        "overview": "Ean has a critical mission to return to the future to save everyone. However, she becomes trapped in the distant past while trying to prevent the escape of alien prisoners who are locked up in the bodies of humans. Meanwhile, Muruk, who helps Ean escape various predicaments, is unnerved when he begins sensing the presence of a strange being in his body. Traveling through the centuries, they are trying to prevent the explosion of the haava.",
+        "popularity": 175.017,
+        "poster_path": "/4RClncz0GTKPZzSAcAalHCw0h3g.jpg",
+        "release_date": "2024-01-10",
+        "title": "Alienoid: Return to the Future",
+        "video": false,
+        "vote_average": 6.781,
+        "vote_count": 506
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/7ks7A3mAO7twDOvBVE4cR8LaIM8.jpg",
+        "genre_ids": [16, 28, 18],
+        "id": 1104844,
+        "original_language": "ja",
+        "original_title": "劇場版ブルーロック -EPISODE 凪-",
+        "overview": "One day, Nagi Seishiro receives an invitation to the mysterious BLUE LOCK Project. What awaits him there is an encounter with the finest strikers assembled from across the country. Nagi's dream of becoming the best, alongside Mikage Reo, will take this prodigy to a world he's never known.",
+        "popularity": 330.274,
+        "poster_path": "/ae434jM5NG2kKX1rRkG5giMhpPI.jpg",
+        "release_date": "2024-04-19",
+        "title": "BLUE LOCK THE MOVIE -EPISODE NAGI-",
+        "video": false,
+        "vote_average": 8.3,
+        "vote_count": 22
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/8UOAYjhwSF3aPZhm6wgLuyHRyrR.jpg",
+        "genre_ids": [18, 37],
+        "id": 932086,
+        "original_language": "en",
+        "original_title": "Horizon: An American Saga - Chapter 1",
+        "overview": "Follow the story of how the Old West was won—and lost—through the blood, sweat and tears of many. Spanning 15 years before, during and following the Civil War from 1859 to 1874, embark on an emotional journey across a country at war with itself, experienced through the lens of families, friends and foes all attempting to discover what it truly means to be the United States of America.",
+        "popularity": 216.164,
+        "poster_path": "/tMI9SGttUJP96GbIIJOAaaCMkFe.jpg",
+        "release_date": "2024-06-26",
+        "title": "Horizon: An American Saga - Chapter 1",
+        "video": false,
+        "vote_average": 6.868,
+        "vote_count": 144
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/9SSEUrSqhljBMzRe4aBTh17rUaC.jpg",
+        "genre_ids": [878, 27, 53],
+        "id": 945961,
+        "original_language": "en",
+        "original_title": "Alien: Romulus",
+        "overview": "While scavenging the deep ends of a derelict space station, a group of young space colonizers come face to face with the most terrifying life form in the universe.",
+        "popularity": 253.741,
+        "poster_path": "/b33nnKl1GSFbao4l3fZDDqsMx0F.jpg",
+        "release_date": "2024-08-14",
+        "title": "Alien: Romulus",
+        "video": false,
+        "vote_average": 0,
+        "vote_count": 0
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/bVigVS3LVotEK4GsBlGOagkjPEc.jpg",
+        "genre_ids": [80, 18],
+        "id": 1008409,
+        "original_language": "en",
+        "original_title": "The Bikeriders",
+        "overview": "After a chance encounter, headstrong Kathy is drawn to Benny, member of Midwestern motorcycle club the Vandals. As the club transforms into a dangerous underworld of violence, Benny must choose between Kathy and his loyalty to the club.",
+        "popularity": 145.424,
+        "poster_path": "/qTb6sSRt8Pw96JTan8ezU2xO0FU.jpg",
+        "release_date": "2024-06-19",
+        "title": "The Bikeriders",
+        "video": false,
+        "vote_average": 7.021,
+        "vote_count": 336
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/4yrOyO3N55XazHQXXYoqiiPQd40.jpg",
+        "genre_ids": [27],
+        "id": 938614,
+        "original_language": "en",
+        "original_title": "Late Night with the Devil",
+        "overview": "A live broadcast of a late-night talk show in 1977 goes horribly wrong, unleashing evil into the nation's living rooms.",
+        "popularity": 200.378,
+        "poster_path": "/jGY62i0D0zitaGz4Ema7vTEYIXw.jpg",
+        "release_date": "2024-03-19",
+        "title": "Late Night with the Devil",
+        "video": false,
+        "vote_average": 7.3,
+        "vote_count": 642
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/Avtx5jsdPuDa091jvx2Lye3ygke.jpg",
+        "genre_ids": [80, 27, 53],
+        "id": 1226578,
+        "original_language": "en",
+        "original_title": "Longlegs",
+        "overview": "In pursuit of a serial killer, an FBI agent uncovers a series of occult clues that she must solve to end his terrifying killing spree.",
+        "popularity": 185.244,
+        "poster_path": "/5aj8vVGFwGVbQQs26ywhg4Zxk2L.jpg",
+        "release_date": "2024-07-10",
+        "title": "Longlegs",
+        "video": false,
+        "vote_average": 6.8,
+        "vote_count": 174
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/73WRlXDUkpFtrB3rFWKnUqxHHDq.jpg",
+        "genre_ids": [27],
+        "id": 1015303,
+        "original_language": "en",
+        "original_title": "Elevator Game",
+        "overview": "Socially awkward teenager Ryan discovers that the night his sister disappeared, she had played 'The Elevator Game' — a ritual conducted in an elevator in which players attempt to travel to another dimension using a set of rules found online. Ignoring warnings, he resolves to follow and find her.",
+        "popularity": 180.197,
+        "poster_path": "/a4Zb13jow8RqJkYFgCUSdmzJCWq.jpg",
+        "release_date": "2023-07-12",
+        "title": "Elevator Game",
+        "video": false,
+        "vote_average": 4.8,
+        "vote_count": 85
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/iAlsYg6dlv1fvOBypM7SldIS1Wl.jpg",
+        "genre_ids": [53, 27],
+        "id": 1032823,
+        "original_language": "en",
+        "original_title": "Trap",
+        "overview": "A father and teen daughter attend a pop concert, where they realize they're at the center of a dark and sinister event.",
+        "popularity": 174.334,
+        "poster_path": "/mWV2fNBkSTW67dIotVTXDYZhNBj.jpg",
+        "release_date": "2024-07-31",
+        "title": "Trap",
+        "video": false,
+        "vote_average": 6.25,
+        "vote_count": 4
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/nv6F6tz7r61DUhE7zgHwLJFcTYp.jpg",
+        "genre_ids": [80, 35, 10749],
+        "id": 974635,
+        "original_language": "en",
+        "original_title": "Hit Man",
+        "overview": "A mild-mannered professor moonlighting as a fake hit man in police stings ignites a chain reaction of trouble when he falls for a potential client.",
+        "popularity": 152.732,
+        "poster_path": "/5BfcYFhRVlgWLFo4SCkQNsIWzyy.jpg",
+        "release_date": "2024-05-16",
+        "title": "Hit Man",
+        "video": false,
+        "vote_average": 7,
+        "vote_count": 603
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/eSGBbCOX7KM3Rf8HHwK8tglklyS.jpg",
+        "genre_ids": [28, 12, 878],
+        "id": 559,
+        "original_language": "en",
+        "original_title": "Spider-Man 3",
+        "overview": "The seemingly invincible Spider-Man goes up against an all-new crop of villains—including the shape-shifting Sandman. While Spider-Man’s superpowers are altered by an alien organism, his alter ego, Peter Parker, deals with nemesis Eddie Brock and also gets caught up in a love triangle.",
+        "popularity": 134.603,
+        "poster_path": "/qFmwhVUoUSXjkKRmca5yGDEXBIj.jpg",
+        "release_date": "2007-05-01",
+        "title": "Spider-Man 3",
+        "video": false,
+        "vote_average": 6.426,
+        "vote_count": 13830
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/k37Ccgu05Am1xxgN5GaW0HX9Kkl.jpg",
+        "genre_ids": [27, 53],
+        "id": 1087388,
+        "original_language": "en",
+        "original_title": "Sting",
+        "overview": "After raising an unnervingly talented spider in secret, 12-year-old Charlotte must face the truth about her pet and fight for her family's survival.",
+        "popularity": 116.704,
+        "poster_path": "/zuSAZIG1PSrxFwPeAlGtg9LTwxo.jpg",
+        "release_date": "2024-04-12",
+        "title": "Sting",
+        "video": false,
+        "vote_average": 6.4,
+        "vote_count": 167
+      }
+    ],
+    "total_pages": 44,
+    "total_results": 875
+  }`;
+  
+const parsedData = JSON.parse(jsonData);
+
+const movieList = document.getElementById('movie-list');
+
+parsedData.results.forEach((movie, index) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const imgSrc = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+  const img = document.createElement('img');
+  img.src = imgSrc;
+  img.alt = movie.title;
+  card.appendChild(img);
+
+  const title = document.createElement('h2');
+  title.textContent = movie.title;
+  card.appendChild(title);
+
+  const releaseDate = document.createElement('p');
+  releaseDate.textContent = `Release Date: ${movie.release_date}`;
+  card.appendChild(releaseDate);
+
+  const rating = document.createElement('span');
+  rating.textContent = `IMDB: ${movie.vote_average}`;
+  card.appendChild(rating);
+
+  const overview = document.createElement('p');
+  overview.textContent = movie.overview;
+  card.appendChild(overview);
+
+  movieList.appendChild(card);
+});
+
+
+  
